862e583273d83b357888d3fdfb804f7ce863db97
DanneryOrtega <dannery19ortega18@gmail.com>
2021-03-20 10:27:06 -0500

    parcial1 commit

--- src/app/operacion/operacion.page.ts
+++ src/app/operacion/operacion.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { isNumber } from 'util';
 
 @Component({
   selector: 'app-operacion',
@@ -28,27 +28,52 @@ export class OperacionPage implements OnInit {
   //     this.resultado=this.mensaje+'multiplicacion entre '+(this.num1)+' y '+(this.num2)+' = '+(parseInt(this.num1)*parseInt(this.num2)).toString();
   // }
 
-  setResult(value) {
-    document.getElementById("result").innerHTML = value;
+ 
+  value = '0';
+  oldValue = '0';
+
+  lastOperator = 'x';
+
+  readyForNewInput = true;
+
+  numberGroups = [
+    [7, 8, 9, 'x'],
+    [4, 5, 6, '-'],
+    [1, 2, 3, '+'],
+    [0, 'c', '/', '=']
+  ];
+  
+  onButtonPress(symbol)
+  {
+    console.log(symbol);
+    if(isNumber(symbol)){
+      console.log('is a number');
+     console.log('is a number');
+      if (this.readyForNewInput)
+        this.value = '' + symbol;
+      else
+        this.value += '' + symbol;
+      this.readyForNewInput = false;
+    }else if(symbol === 'c'){
+      this.value = '0';
+      this.readyForNewInput = true;
+    }else if(symbol === '='){
+        if (this.lastOperator === 'x')
+        this.value = '' + (parseInt(this.oldValue) * parseInt(this.value));
+      else if (this.lastOperator === '-')
+        this.value = '' + (parseInt(this.oldValue) - parseInt(this.value));
+      else if (this.lastOperator === '+')
+        this.value = '' + (parseInt(this.oldValue) + parseInt(this.value));
+      else if (this.lastOperator === '/')
+        this.value = '' + (parseInt(this.oldValue) / parseInt(this.value));
+      this.readyForNewInput = true;
+    }else{
+      this.readyForNewInput = true;
+      this.oldValue = this.value;
+      this.lastOperator = symbol;
+    }
   }
 
-  getResult() {
-    return(document.getElementById("result").innerHTML);
-  }
 
-  add(key) {
-    var result = this.getResult();
-    if (result!="0" || isNaN(key)) this.setResult(result + key);
-    else this.setResult(key);
-  }
-
-  calc() {
-    var result = eval(this.getResult());
-    this.setResult(result);
-  }
-
-  del() {
-    this.setResult(0);
-  }
 
 }
